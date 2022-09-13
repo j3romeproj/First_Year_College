b02ae3bf7e57b95c3d3cf3112c8f06f075bbe670
j3romeproj <111049617+j3romeproj@users.noreply.github.com>
2022-09-13 13:06:56 +0800

    Add files via upload

---
+++ Alpa_A1_3.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+int x[10];
+int i, n, Num;
+char ans = 'Y';
+
+main()
+{   // Input integer value
+	for (i = 0; i < 10; i++){
+		printf(" Enter an integer value: ");
+		scanf("%d", &x[i]);
+	}
+
+    // Display all integer value
+	printf("\n List contains: ");
+	for (i = 0; i < 10; i++)
+        printf("%3d", x[i]);
+
+    // Control statement to know if the user wants to search a number again
+    while (ans == 'Y' || ans == 'y'){
+        printf("\n\n Enter the number you want to search: "); // Input a number to be searched
+            scanf( "%d", &n);
+
+        // Display the number that you want to search
+        printf("\n\t Number to be searched is %d\n", n);
+
+        // Statement used to check if the number is in the list
+        Num = 0;
+        for (i = 0; i < 10; i++){
+            if (x[i] == n)
+                Num++;
+        }
+
+        // Display how many occurence the number appear in the list
+        printf("\n\t There are %d occurence/s of the number %d\n", Num, n);
+
+        // Input yes or no (Y/N) if you want to search again
+        printf("\n Would you like to search again? (Y/N): ");
+            scanf("\n");
+            scanf("%c", &ans);
+    }
+
+getch();
+return 0;
+}
